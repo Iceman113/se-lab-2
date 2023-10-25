a20412bec4d503f5b1d3c1cc418945fd4c6d0f3d
unknown <176619@BME.HU>
2023-10-25 17:38:51 +0200

    Tests are made and all of them works, started to write junit test for TorpedoStore

--- src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
+++ src/test/java/hu/bme/mit/spaceship/TorpedoStoreTest.java
@@ -16,5 +16,45 @@ class TorpedoStoreTest {
 
     // Assert
     assertEquals(true, result);
+
+    
   }
+  @Test
+
+  void empty_Check(){
+    TorpedoStore store = new TorpedoStore(0);
+    assertEquals(true, store.isEmpty());
+  }
+
+  @Test
+  void getTorpedoCount_Works(){
+    TorpedoStore store = new TorpedoStore(1);
+    assertEquals(1, store.getTorpedoCount());
+  }
+  @Test
+  void fire_DeniedByFailure(){
+
+    TorpedoStore store = new TorpedoStore(1, 100.0);
+
+    // Act
+    boolean result = store.fire(1);
+
+    // Assert
+    assertEquals(false, result);
+
+  }
+
+  @Test
+  void fire_DeniedByNegativeTorpedo(){
+    TorpedoStore store = new TorpedoStore(-1);
+
+    // Act
+    boolean result = store.fire(1);
+
+    // Assert
+    assertEquals(false, result);
+  }
+
 }
+
+
